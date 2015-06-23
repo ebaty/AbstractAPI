dd30a79a879b5987f0d3e13cf58e151a4b8944a6
ebaty <ebamasa1220@gmail.com>
2015-06-24 01:10:31 +0900

    create project and Carthfile

---
+++ AbstractAPI/API.swift
@@ -0,0 +1,13 @@
+//
+//  API.swift
+//  AbstractAPI
+//
+//  Created by Masaki EBATA on 2015/06/24.
+//  Copyright (c) 2015年 Masaki EBATA. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import ObjectMapper
+import Result
+
