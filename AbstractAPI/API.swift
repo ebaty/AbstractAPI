9d250713b2db9df12eb207513a355d14fc52db8d
ebaty <ebamasa1220@gmail.com>
2015-06-24 01:41:01 +0900

    great code...

--- AbstractAPI/API.swift
+++ AbstractAPI/API.swift
@@ -11,3 +11,22 @@ import Alamofire
 import ObjectMapper
 import Result
 
+protocol API {
+    typealias ResponseType
+    typealias ErrorType
+
+    var method: Alamofire.Method { get }
+    var pathString: String { get }
+    var parameters: [String:AnyObject] { get }
+    var responseParser: (AnyObject?, NSError?) -> Result<ResponseType, ErrorType> { get }
+}
+
+class APIClient {
+    static func sendRequest<T: API where T.ResponseType: Mappable>(API: T, handler: Result<T.ResponseType, T.ErrorType> -> ()) {
+        Alamofire.request(API.method, API.pathString, parameters: API.parameters) .responseJSON { (request, response, data, error) in
+            handler( API.responseParser(data, error) )
+        }
+    }
+}
+
+class APIs {}
